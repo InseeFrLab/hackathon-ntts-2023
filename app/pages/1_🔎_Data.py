5152a3500fa9cca2408599582bfa3d5923151db1
lmalherbe <lucas.malherbe@insee.fr>
2023-03-06 03:10:43 +0000

    update app

--- app/pages/1_🔎_Data.py
+++ app/pages/1_🔎_Data.py
@@ -2,10 +2,156 @@ import streamlit as st
 import plotly.express as px
 import numpy as np
 from streamlit_utils import *
-
+import time
+import plotly.graph_objects as go
+import pandas as pd
 
 st.set_page_config(page_title="The dataset", page_icon="🔎")
 
 add_logos()
 
 st.title("🔎 The dataset")
+
+# Daily transactions
+
+df_daily = read_csv_s3('projet-hackathon-ntts-2023/data-hackathon/Daily_data.csv')
+df_daily=df_daily.sort_values('day')
+fig0 = px.line(df_daily, x="day", y="Number", title="Credit card daily transactions")
+#st.plotly_chart(fig0, theme="streamlit", use_container_width=True)
+
+chart = st.plotly_chart(fig0, use_container_width=True)
+for i in range(len(df_daily)):
+    df_subset = df_daily.iloc[:i+1]
+    fig0.data[0].x = df_subset['day']
+    fig0.data[0].y = df_subset['Number']
+    fig0.update_layout(title="Credit card daily transactions")
+    chart.plotly_chart(fig0)
+    time.sleep(0.001)
+
+# Share of amount by type of payment NO panel data
+
+df_type_payment = read_csv_s3('projet-hackathon-ntts-2023/data-hackathon/Cash_Online_offline_nopanel.csv')
+df_type_payment=df_type_payment.sort_values('Mois')
+df_type_payment.rename(columns = {'Mois':'Month'}, inplace = True)
+df_type_payment.sort_values(['Month', 'PaymentType'], inplace = True)
+fig1 = px.line(df_type_payment, x="Month", y="ShareAmount",color='PaymentType', title="Share of amount by type of payment - All users")
+st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
+
+# Share of amount by type of payment panel data
+
+df_type_payment_pan = read_csv_s3('projet-hackathon-ntts-2023/data-hackathon/Cash_Online_offline_panel.csv')
+df_type_payment_pan=df_type_payment_pan.sort_values('Mois')
+df_type_payment_pan.rename(columns = {'Mois':'Month'}, inplace = True)
+df_type_payment_pan.sort_values(['Month', 'PaymentType'], inplace = True)
+fig2 = px.line(df_type_payment_pan, x="Month", y="ShareAmount",color='PaymentType', title="Share of amount by type of payment - Panel")
+st.plotly_chart(fig2, theme="streamlit", use_container_width=True)
+
+
+#### Share activity per sector#########
+
+
+mcc_groups2 = read_csv_s3('projet-hackathon-ntts-2023/data-hackathon/mcc_groups2.csv')
+
+amount = read_csv_s3('projet-hackathon-ntts-2023/data-hackathon/amount.csv')
+amount = amount.merge(mcc_groups2, on="MCC", how="left")
+amount = amount.loc[~amount['MCC_group2'].isna()]
+amount = amount.groupby('MCC_group2').agg(amount=('amount', 'sum')).reset_index()
+amount['amount_rate'] = 100 * amount['amount'] / amount['amount'].sum()
+amount = amount.sort_values('amount_rate', ascending=False)
+
+amount_panel = read_csv_s3('projet-hackathon-ntts-2023/data-hackathon/amount_panel.csv')
+amount_panel = amount_panel.merge(mcc_groups2, on="MCC", how="left")
+amount_panel = amount_panel.loc[~amount_panel['MCC_group2'].isna()]
+amount_panel = amount_panel.groupby('MCC_group2').agg(amount=('amount', 'sum')).reset_index()
+amount_panel['amount_rate'] = 100 * amount_panel['amount'] / amount_panel['amount'].sum()
+amount_panel = amount_panel.sort_values('amount_rate', ascending=False)
+
+# Créer le graphique donut chart
+fig1 = go.Figure(data=[go.Pie(labels=amount['MCC_group2'],
+                             values=amount['amount_rate'],
+                             hole=.5)])
+fig1.update_layout(title='Share of amount by great sectors - all users',
+                  annotations=[dict(text=' ',
+                                    font_size=20,
+                                    showarrow=False)])
+
+# Créer le graphique donut chart
+fig2 = go.Figure(data=[go.Pie(labels=amount_panel['MCC_group2'],
+                             values=amount_panel['amount_rate'],
+                             hole=.5)])
+fig2.update_layout(title='Share of amount by great sectors - panel',
+                  annotations=[dict(text=' ',
+                                    font_size=20,
+                                    showarrow=False)])
+
+col1, col2 = st.columns(2)
+with col1:
+    st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
+with col2:
+    st.plotly_chart(fig2, theme="streamlit", use_container_width=True)
+
+
+
+#####################
+
+# Share of amount by age group NO panel data
+
+df_age_no = read_csv_s3('projet-hackathon-ntts-2023/data-hackathon/agegroup_nopanel.csv')
+df_age_no =df_age_no.sort_values('AgeGroup')
+fig3 = px.bar(df_age_no, x='AgeGroup', y='ShareAmount', title="Share of amount by age group - All users")
+#st.plotly_chart(fig3, theme="streamlit", use_container_width=True)
+
+# Share of amount by age group panel data
+
+df_age_pan = read_csv_s3('projet-hackathon-ntts-2023/data-hackathon/agegroup_panel.csv')
+df_age_pan =df_age_pan.sort_values('AgeGroup')
+fig4 = px.bar(df_age_pan, x='AgeGroup', y='ShareAmount', title="Share of amount by age group - Panel")
+#st.plotly_chart(fig4, theme="streamlit", use_container_width=True)
+
+
+col1, col2 = st.columns(2)
+with col1 : 
+    st.plotly_chart(fig3, theme="streamlit", use_container_width=True)
+with col2 :
+    st.plotly_chart(fig4, theme="streamlit", use_container_width=True)
+
+
+# heatmap NO panel  country
+
+data=[[63, 35, 0, 0, 1], [1, 98, 0, 0, 1], [2, 34, 58, 1, 5],[1, 29, 2, 43, 25]]
+fig6 = px.imshow(data,
+                labels=dict(x="User country", y="Merchant country", color="Amount share"),
+                x=['Austria', 'Deutschland', 'Spain', 'France', 'Other'],
+                y=['Austria', 'Deutschland', 'Spain', 'France'],
+                text_auto=True,
+                title='Share of amount - All users'
+               )
+fig6.update_xaxes(side="top")
+st.plotly_chart(fig6, theme="streamlit", use_container_width=True)
+
+# heatmap panel  country
+
+data=[[59, 40, 0, 0, 1], [1, 98, 0, 0, 1], [4, 89, 3, 2, 2],[2, 47, 0, 47, 4]]
+fig5 = px.imshow(data,
+                labels=dict(x="User country", y="Merchant country", color="Amount share"),
+                x=['Austria', 'Deutschland', 'Spain', 'France', 'Other'],
+                y=['Austria', 'Deutschland', 'Spain', 'France'],
+                text_auto=True,
+                title='Share of amount - Panel'
+               )
+fig5.update_xaxes(side="top")
+st.plotly_chart(fig5, theme="streamlit", use_container_width=True)
+
+# col1, col2 = st.columns(2)
+# with col1 : 
+#     st.plotly_chart(fig6, theme="streamlit", use_container_width=True)
+# with col2 :
+#     st.plotly_chart(fig5, theme="streamlit", use_container_width=True)
+
+
+# Map : Share of accommodation expenditures in total spending
+
+st.subheader('Share of accommodation expenditures in total spending')
+
+map_html = read_html_s3('projet-hackathon-ntts-2023/data-hackathon/map_accomadation_expenditures_rate_nuts3_v2.html')
+st.components.v1.html(map_html, height=700)
--- app/pages/3_🚨_Alerts_by_sector.py
+++ app/pages/3_🚨_Alerts_by_sector.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objs as go
 import numpy as np
-
+import datetime
 
 st.set_page_config(page_title="Alerts by sector", page_icon="🚨")
 
@@ -14,17 +14,23 @@ add_logos()
 
 
 df_agg = read_csv_s3("projet-hackathon-ntts-2023/data-hackathon/data_agg_MCC_Month_anomaly_3levels.csv")
-#df_95 = read_csv_s3("projet-hackathon-ntts-2023/data-hackathon/data_agg_MCC_Month_anomaly95.csv")
-#df_99 = read_csv_s3("projet-hackathon-ntts-2023/data-hackathon/data_agg_MCC_Month_anomaly95.csv")
 
 df_agg['TxnMonth'] = pd.to_datetime(df_agg['TxnMonth'])
+
+df_agg['Alert level'] = ""
+df_agg['Up or Down'] = ""
+df_agg.loc[df_agg['anomaly'].isin([-3, 3]), 'Alert level'] = "3 - Severe"
+df_agg.loc[df_agg['anomaly'].isin([-2, 2]), 'Alert level'] = "2 - High"
+df_agg.loc[df_agg['anomaly'].isin([-1, 1]), 'Alert level'] = "1 - Medium"
+df_agg.loc[df_agg['anomaly']>0, 'Up or Down'] = "Up"
+df_agg.loc[df_agg['anomaly']<0, 'Up or Down'] = "Down"
 
 chosen_MCC = st.selectbox(
     'Sector',
     df_agg[['MCCGroup', 'Amount']].groupby('MCCGroup').sum('Amount').sort_values('Amount', ascending = False).index
 )
 
-df_agg_single_MCC = df_agg[df_agg['MCCGroup'] == chosen_MCC]
+df_agg_single_MCC = df_agg.loc[df_agg['MCCGroup'] == chosen_MCC, :]
 
 #st.line_chart(df_agg_single_MCC[['TxnMonth', 'Amount']], x='TxnMonth', y='Amount')
 
@@ -67,6 +73,19 @@ df_agg_single_MCC = df_agg[df_agg['MCCGroup'] == chosen_MCC]
 # create line graph with color based on 'anomaly' column
 #fig = px.line(df, x='x', y='y', color='anomaly', color_discrete_map=color_map)
 
+#Checkboxes for confidence intervals
+col1, col2, col3, col4 = st.columns([2, 1, 1, 1])
+
+with col1:
+    st.write("Show confidence intervals")
+with col2:
+    ci_90 = st.checkbox('90%', value = True)
+with col3:
+    ci_95 = st.checkbox('95%', value = True)
+with col4:
+    ci_99 = st.checkbox('99%', value = True)
+
+
 # create line graph
 fig = px.line(df_agg_single_MCC.sort_values("TxnMonth"), x="TxnMonth", y="Amount", 
 title=f'Total spend amount - {chosen_MCC}')
@@ -80,10 +99,11 @@ fig.add_trace(px.scatter(df_filtered, x='TxnMonth', y='Amount', color_discrete_s
 #fig.add_trace(px.scatter(df_agg_single_MCC, x='TxnMonth', y='Amount', color='anomaly').data[0])
 
 #99% confidence intervals
-fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_upper99'],
+if ci_99:
+    fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_upper99'],
                     line = dict(color='rgba(255,0,0,0.4)', dash='dash')))
 
-fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_lower99'],
+    fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_lower99'],
                     line = dict(color='rgba(255,0,0,0.4)', dash='dash')))
 
 # 95% anomalies
@@ -92,11 +112,12 @@ fig.add_trace(px.scatter(df_filtered, x='TxnMonth', y='Amount', color_discrete_s
 df_filtered = df_agg_single_MCC[df_agg_single_MCC['anomaly'] == -2]
 fig.add_trace(px.scatter(df_filtered, x='TxnMonth', y='Amount', color_discrete_sequence=['orange']).data[0])
 
+if ci_95:
 # 95% confidence intervals
-fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_upper95'],
+    fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_upper95'],
                     line = dict(color='rgba(255,69,0, 0.4)', dash='dash')))
 
-fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_lower95'],
+    fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_lower95'],
                     line = dict(color='rgba(255,69,0, 0.4)', dash='dash')))
 
 # 90% anomalies
@@ -106,23 +127,58 @@ fig.add_trace(px.scatter(df_filtered, x='TxnMonth', y='Amount', color_discrete_s
 df_filtered = df_agg_single_MCC[df_agg_single_MCC['anomaly'] == -1]
 fig.add_trace(px.scatter(df_filtered, x='TxnMonth', y='Amount', color_discrete_sequence=['yellow']).data[0])
 
+if ci_90:
 # 90% confidence intervals
-fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_upper90'],
+    fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_upper90'],
                     line = dict(color='rgba(204,204,0, 0.4)', dash='dash')))
 
-fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_lower90'],
+    fig.add_trace(go.Scatter(x=df_agg_single_MCC['TxnMonth'], y=df_agg_single_MCC['yhat_lower90'],
                     line = dict(color='rgba(204,204,0, 0.4)', dash='dash')))
-
-
-#df_95['TxnMonth'] = pd.to_datetime(df_95['TxnMonth'])
-#df_agg_single_MCC = df_agg[df_agg['MCCGroup'] == chosen_MCC]
-
-#df_filtered = df_agg_single_MCC[df_agg_single_MCC['anomaly'] == 1]
-#fig.add_trace(px.scatter(df_filtered, x='TxnMonth', y='Amount', color_discrete_sequence=['orange']).data[0])
-#df_filtered = df_agg_single_MCC[df_agg_single_MCC['anomaly'] == -1]
-#fig.add_trace(px.scatter(df_filtered, x='TxnMonth', y='Amount', color_discrete_sequence=['orange']).data[0])
-
 
 fig.update_traces(showlegend=False, marker={'size': 8})
 
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+
+
+df_agg_single_MCC_anomaly = df_agg_single_MCC.loc[df_agg_single_MCC['anomaly']!=0, ['TxnMonth', 'Alert level', 'Up or Down']]
+df_agg_single_MCC_anomaly = df_agg_single_MCC_anomaly.rename(columns = {'TxnMonth': 'Month'})
+df_agg_single_MCC_anomaly["Month"] = df_agg_single_MCC_anomaly["Month"].astype(str)
+
+st.markdown(f"""
+### Alerts for {chosen_MCC}
+"""
+)
+
+#Checkboxes for confidence intervals
+col1, col2, col3, col4, col5 = st.columns([1.5, 1, 0.77, 1, 2])
+
+with col1:
+    st.write("Choose alert levels")
+with col2:
+    Medium = st.checkbox('Medium', value = True)
+with col3:
+    High = st.checkbox('High', value = True)
+with col4:
+    Severe = st.checkbox('Severe', value = True)
+
+
+if not Medium:
+    df_agg_single_MCC_anomaly = df_agg_single_MCC_anomaly.loc[df_agg_single_MCC_anomaly["Alert level"]!="1 - Medium", :]
+if not High:
+    df_agg_single_MCC_anomaly = df_agg_single_MCC_anomaly.loc[df_agg_single_MCC_anomaly["Alert level"]!="2 - High", :]
+if not Severe:
+    df_agg_single_MCC_anomaly = df_agg_single_MCC_anomaly.loc[df_agg_single_MCC_anomaly["Alert level"]!="3 - Severe", :]
+
+st.write(df_agg_single_MCC_anomaly)
+
+st.markdown("## All MCCs in alert")
+
+year = st.selectbox('Year', range(2018, 2024))
+
+month = st.selectbox('Month', range(1, 13))
+
+df_agg = df_agg.loc[(df_agg["anomaly"]!=0) & (df_agg['TxnMonth'] == pd.Timestamp(f"{year}-{month}-01")), ['TxnMonth', 'MCCGroup', 'Alert level', 'Up or Down']]
+df_agg = df_agg.rename(columns = {'TxnMonth': 'Month', 'MCCGroup': 'Sector'})
+
+st.dataframe(df_agg)
